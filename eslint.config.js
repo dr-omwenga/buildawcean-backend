ea35ea30c0a3a6ad3c4dd9ded3b89bb768c8fc4a
Dr Omwenga <dr.omwenga.dev@gmail.com>
2026-04-18 15:48:32 +0300

    Add Eslint

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: globals.node,
+    },
+    rules: {
+      // Add custom rules here
+    },
+  },
+];
