f3196003757c379a799ed78e4a275c7be62ff333
Dr Omwenga <dr.omwenga.dev@gmail.com>
2026-04-20 13:25:56 +0300

    Add expand=estimatedDeliveryTime to Get /delivery-options API.

--- src/controllers/deliveryOptionControllers.js
+++ src/controllers/deliveryOptionControllers.js
@@ -1,8 +1,26 @@
 import DeliveryOption from "../models/deliveryOption.js";
 
+const DAY_IN_MS = 24 * 60 * 60 * 1000;
+
 export const getDeliveryOptions = async (req, res, next) => {
   try {
     const deliveryOptions = await DeliveryOption.findAll();
+    const expandValue = String(req.query.expand || "").toLowerCase();
+
+    if (expandValue === "estimateddeliverytime") {
+      const nowMs = Date.now();
+      const expandedDeliveryOptions = deliveryOptions.map((deliveryOption) => {
+        const option = deliveryOption.toJSON();
+
+        return {
+          ...option,
+          estimatedDeliveryTimeMs: nowMs + (option.deliveryDays * DAY_IN_MS)
+        };
+      });
+
+      return res.json({ success: true, data: expandedDeliveryOptions });
+    }
+
     res.json({ success: true, data: deliveryOptions });
   } catch (err) {
     next(err);
